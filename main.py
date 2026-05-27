dd16a363e683a5d789e836d00c2483d5f123271e
an3243 <an3243@drexel.edu>
2026-05-27 17:10:04 -0400

    Drawing surface.

--- main.py
+++ main.py
@@ -6,3 +6,24 @@ WIDTH = 1280
 HEIGHT = 720
 
 display_surace = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+
+# Create surface
+surf = pygame.Surface(( 100, 200 ))
+
+# Drawing Screen
+while running:
+    
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    # draw the game
+    display_surace.fill('darkgray')
+    display_surace.blit(surf)
+    pygame.display.update()
+
+pygame.quit()
